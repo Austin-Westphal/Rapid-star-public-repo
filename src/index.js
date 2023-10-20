e41875a813e28eca7f545ae7960557efc4f662bd
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2023-10-20 10:56:58 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ if (process.env.TOPGG_TOKEN) {
 console.clear();
 console.log(chalk.blue(chalk.bold(`System`)), (chalk.white(`>>`)), (chalk.green(`Starting up`)), (chalk.white(`...`)))
 console.log(`\u001b[0m`)
-console.log(chalk.red(`© CorwinDev | 2021 - ${new Date().getFullYear()}`))
+console.log(chalk.red(`© RapidDevs | 2021 - ${new Date().getFullYear()}`))
 console.log(chalk.red(`All rights reserved`))
 console.log(`\u001b[0m`)
 console.log(`\u001b[0m`)
