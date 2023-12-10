705afb94c9dcd4afe1a3cab72f1ca3d25cde11e4
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2023-12-10 10:09:06 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ require('dotenv').config('./.env');
 const axios = require('axios');
 // Check if is up to date
 const { version } = require('.././package.json');
-axios.get('https://api.github.com/repos/CorwinDev/Discord-Bot/releases/latest').then(res => {
+axios.get('https://api.github.com/repos/Austin-Westphal/rapid-star-bot-v1-0-0/releases/latest').then(res => {
     if (res.data.tag_name !== version) {
         console.log(chalk.red.bgYellow(`Your bot is not up to date! Please update to the latest version!`, version + ' -> ' + res.data.tag_name));
     }
