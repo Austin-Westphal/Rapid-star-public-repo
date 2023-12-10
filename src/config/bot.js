bef2a9e2ee92f6e1aaff88228051e820eb7b1e97
Austin Westphal <78154173+Austin-Westphal@users.noreply.github.com>
2023-12-10 12:16:44 -0500

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -8,9 +8,9 @@ module.exports = {
     discord: {
         id: process.env.DISCORD_ID,
         prefix: '!',
-        footer: `© Corwin 2021 - ${new Date().getFullYear()}`, 
+        footer: `© RapidDevs 2021 - ${new Date().getFullYear()}`, 
         botInvite: `https://discord.com/oauth2/authorize?&client_id=840212110817755157&scope=applications.commands+bot&permissions=8`,
-        serverInvite: "https://discord.gg/qHXxpQKC2H",
+        serverInvite: "https://discord.gg/",
     },
 
     wordList: `Airplane
